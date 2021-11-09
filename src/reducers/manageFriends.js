04ed62c314b1ec017a3ae571467f76e4c0ef6b11
zdurr22 <zdurr22@gmail.com>
2021-11-09 12:49:47 -0500

    commit tree 0e2eb0bf67334bbca9bd453b9ec0c6ce07bf699a

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+    friends: [],
+}, action){
+    switch (action.type) {
+        case "ADD_FRIEND":
+            return (
+                {...state, 
+                friends: [
+                    ...state.friends,
+                    action.friend
+                ]})
+        case "REMOVE_FRIEND":
+            const removalInd = state.friends.findIndex(friend => friend.id === action.id);
+            return(
+                {...state,
+                friends: [
+                    ...state.friends.slice(0, removalInd),
+                    ...state.friends.slice(removalInd + 1)
+                ]}
+            )
+        default:
+        return state;
+    }
 }
